2899cef6c37010e5f800cbd86150ef5a1f12b310
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-03-02 21:37:43 -0400

    добавлено Лаб.1, упр. 2

---
+++ ITMO.ASP.NET-Lab.01-Ex.01-WebAppCoreProduct/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿// Лаб.1, упр.2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITMO.ASP.NET_Lab._01_Ex._01_WebAppCoreProduct.Models
+{
+	public class Product
+	{
+		public string Name { get; set; }
+		public decimal? Price { get; set; }
+	}
+}
--- ITMO.ASP.NET-Lab.01-Ex.01-WebAppCoreProduct/Pages/Index.cshtml.cs
+++ ITMO.ASP.NET-Lab.01-Ex.01-WebAppCoreProduct/Pages/Index.cshtml.cs
@@ -4,23 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using ITMO.ASP.NET_Lab._01_Ex._01_WebAppCoreProduct.Models;    // Ћаб.1, упр.2
+                                                               
 namespace ITMO.ASP.NET_Lab._01_Ex._01_WebAppCoreProduct.Pages
 {
     public class IndexModel : PageModel
     {
-        public string Name { get; set; }
-        public decimal? Price { get; set; }
+        
         public bool IsCorrect { get; set; } = true;
+        public Product Product { get; set; }
         public void OnGet(string name, decimal? price)
         {
+            Product = new Product();
             if (price == null || price < 0 || string.IsNullOrEmpty(name))
             {
                 IsCorrect = false;
                 return;
             }
-            Price = price;
-            Name = name;
+            Product.Price = price;
+            Product.Name = name;
         }
     }
 }

fb464e0e17f0873dcba992881db9df0165ecd0f3
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-03-11 12:37:30 +0300

    добвлена Лаб. 1 MVC

---
+++ ITMO.ASP.NET-Practice-MVC-Lab.01-WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITMO.ASP.NET_Practice_MVC_Lab._01_WebMVCR1.Models;
+
+namespace ITMO.ASP.NET_Practice_MVC_Lab._01_WebMVCR1.Controllers
+{
+	public class HomeController : Controller
+	{
+		//public IActionResult Index()
+		//{
+		//	return View();
+		//}
+
+		public string Index(string hel)
+		{
+			string Greeting = ModelClass.ModelHello() + ", " + hel;
+			return Greeting;
+		}
+	}
+}
---
+++ ITMO.ASP.NET-Practice-MVC-Lab.01-WebMVCR1/Models/ModelClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITMO.ASP.NET_Practice_MVC_Lab._01_WebMVCR1.Models
+{
+	public class ModelClass
+	{
+		public static string ModelHello()
+		{
+			int hour = DateTime.Now.Hour;
+			string Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+			return Greeting;
+		}
+	}
+}
